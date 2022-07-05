59195e11a1a2962063a70557d668301f4a7d9daf
Almog Shtaigmann <a.shtaigmann@gmail.com>
2022-07-05 14:38:30 +0300

    Adding Slom' Script from get grade by course id

---
+++ FirstTry.py
@@ -0,0 +1,34 @@
+# bot.py
+import os
+import random
+
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.command(name='98')
+async def nine_nine(ctx):
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    response = random.choice(brooklyn_99_quotes)
+    await ctx.send("Almof")
+
+bot.command(name='95')
+async def Almog(ctx):
+    brooklyn_99_quotes =['My name is almog!']
+
+    response = random.choice(brooklyn_99_quotes)
+    await ctx.send(response)
+
+bot.run(TOKEN)
--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+import os
 
+# Get the value of 'HOME'
+# environment variable
+key = 'HOME'
+value = os.getenv(key)
+
+# Print the value of 'HOME'
+# environment variable
+print("Value of 'HOME' environment variable :", value)
