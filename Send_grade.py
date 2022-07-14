340450cd8c62c7c7c891630ac0646643dfd5ffbb
Almog Shtaigmann <a.shtaigmann@gmail.com>
2022-07-15 00:44:47 +0300

    add bot features

--- Send_grade.py
+++ Send_grade.py
@@ -8,64 +8,66 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+def getLS():
+    def clearConsole():
+        command = 'clear'
+        if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
+            command = 'cls'
+        os.system(command)
 
-def clearConsole():
-    command = 'clear'
-    if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
-        command = 'cls'
-    os.system(command)
-
-clearConsole()
-
-chrome_options = Options()
-chrome_options.add_experimental_option("detach", True)
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--disable-gpu')
-chrome_options.add_argument("--log-level=0")
-warnings.filterwarnings("ignore", category=DeprecationWarning)
-browser = webdriver.Chrome(options=chrome_options ,\
-    executable_path=r'C:/LOCATIONOFEXE/chromedriver_win32/chromedriver.exe')
-
-webpage = 'https://sheilta.apps.openu.ac.il/pls/dmyopt2/course_info.courses?p_from=1'
-browser.get(webpage)
-
-clearConsole()
-
-browser.find_element_by_id("p_user").send_keys("USER")
-
-browser.find_element_by_id("p_sisma").send_keys("PASSWORD")
-
-browser.find_element_by_id("p_mis_student").send_keys("TEUDATZEUT")
-
-browser.find_element_by_xpath('//*[@id="login_sso"]/fieldset/input[1]').click()
-
-while True:
-    browser.get(webpage)
-    html = browser.page_source
-    soup = BeautifulSoup(html,'html.parser')
-    tr_table=soup.find("table", {'class':'content_tbl'}).text
-    items=tr_table.split('\n')
-    course_id=input("Enter Course ID: ") or '30204'
-    for i in range(len(items)):
-        if course_id in items[i]:
-            if items[i-9].isdigit():
-                result = "{0} Course {1} - Test grade is: {2}".format(datetime.now().strftime('%d/%m/%Y %H:%M:%S'),course_id,items[i-9])
-                if items[i-12].isdigit():
-                    result += "\n{0} Final grade is: {1}".format(datetime.now().strftime('%d/%m/%Y %H:%M:%S'),items[i-12])
-                else:
-                    result += "\n{0} Final grade not given yet - good luck MOED B".format(datetime.now().strftime('%d/%m/%Y %H:%M:%S'))
-                print(result)
-                message= { "value1" : result }
-                IFTTT_KEY='KEY'
-                IFTTT_URL='https://maker.ifttt.com/trigger/{EVENT_NAME_IN_IFTTT}/with/key/'+IFTTT_KEY
-                requests.post(IFTTT_URL,data=message)
-                exit()
-            else:
-                print("{0} No grade yet for Course {1}".format(datetime.now().strftime('%d/%m/%Y %H:%M:%S'),course_id))
-            break
-
-    time.sleep(120)
-    browser.refresh()
     clearConsole()
 
-browser.quit()
+    chrome_options = Options()
+    chrome_options.add_experimental_option("detach", True)
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument("--log-level=0")
+    warnings.filterwarnings("ignore", category=DeprecationWarning)
+    browser = webdriver.Chrome(options=chrome_options ,\
+        executable_path=r'/Users/almogshtaigmann/Downloads/chromedriver')
+
+    webpage = 'https://sheilta.apps.openu.ac.il/pls/dmyopt2/course_info.courses?p_from=1'
+    browser.get(webpage)
+
+    clearConsole()
+
+    browser.find_element("id", 'p_user').send_keys("SHALMO4")
+
+    browser.find_element("id","p_sisma").send_keys("AlmogOpen98")
+
+    browser.find_element("id","p_mis_student").send_keys("209401553")
+
+    browser.find_element("xpath",'//*[@id="login_sso"]/fieldset/input[1]').click()
+
+    while True:
+        browser.get(webpage)
+        html = browser.page_source
+        soup = BeautifulSoup(html,'html.parser')
+        tr_table=soup.find("table", {'class':'content_tbl'}).text
+        items=tr_table.split('\n')
+        print(items)
+        course_id = '20441'
+        for i in range(len(items)):
+            if course_id in items[i]:
+                if items[i-9].isdigit():
+                    result = "{0} Course {1} - Exam grade is: {2}".format(datetime.now().strftime('%d/%m/%Y %H:%M:%S'),course_id,items[i-9])
+                    if items[i-12].isdigit():
+                        result += "\n{0} Final grade in course {2} is: {1}".format(datetime.now().strftime('%d/%m/%Y %H:%M:%S'),items[i-12],course_id)
+                    else:
+                        result += "\n{0} Final grade not given yet - good luck MOED B".format(datetime.now().strftime('%d/%m/%Y %H:%M:%S'))
+                    print(result)
+                    return  result
+                    message= { "value1" : result }
+                    IFTTT_KEY='KEY'
+                    IFTTT_URL='https://maker.ifttt.com/trigger/{EVENT_NAME_IN_IFTTT}/with/key/'+IFTTT_KEY
+                    requests.post(IFTTT_URL,data=message)
+                    exit()
+                else:
+                    print("{0} No grade yet for Course {1}".format(datetime.now().strftime('%d/%m/%Y %H:%M:%S'),course_id))
+                break
+
+        time.sleep(120)
+        browser.refresh()
+        clearConsole()
+
+    browser.quit()
---
+++ t_Bot.py
@@ -0,0 +1,55 @@
+import os
+import discord
+import requests
+import json
+from Send_grade import getLS
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+def get_qoute():
+    respone = requests.get("https://zenquotes.io/api/random")
+    js_data = json.loads(respone.text)
+    quote = js_data[0]['q']
+    return quote
+
+
+client = discord.Client()
+
+
+@client.event
+async  def on_ready():
+    print(f'We have logged in as {client.user}')
+botFeatures = [
+    '$hello - Just say Hi 😀🔥' ,
+    '$getLastGrade - get the last grade 📚'
+]
+@client.event
+async def on_message(msg):
+    if msg.author == client.user:
+        return
+
+    if msg.content.startswith('$ShowAll'):
+
+        for task in botFeatures:
+            await msg.channel.send(task)
+            print(task)
+
+    if msg.content.startswith('$hello'):
+        await msg.channel.send("Hello Almog! Im your Grade bot 🤖🦾, I will send you updates for your grade! Good luck!")
+        tstmsg()
+
+    if msg.content.startswith('$getLastGrade'):
+        grade = getLS()
+        await msg.channel.send(grade)
+
+    if msg.content.startswith('$Quote'):
+        await msg.channel.send(get_qoute())
+
+def tstmsg():
+    client.get_channel()
+    on_message('$Quote')
+
+
+client.run(TOKEN)
